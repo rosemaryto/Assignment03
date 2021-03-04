64be9d256455728183397a4bd5d370e64e3bd208
rosemaryto <rosemary_to@ymail.com>
2021-03-03 22:12:57 -0800

    q4Done

---
+++ beginning/js/04-odd-or-even.js
@@ -0,0 +1,8 @@
+let i;
+for (i = 0; i < 16; i += 1) {
+    if (i % 2 == 0) {
+        window.document.write(i + " is even" + "<br>");
+    } else {
+        window.document.write(i + " is odd" + "<br>");
+    }
+}
